5d9b95252c5ad3a5a7081cd0d4f292083a200184
Takudzwa-D <takudzwadmavindidze@gmail.com>
2026-04-28 15:12:14 +0200

    first commit

---
+++ config/cors.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * CORS compatibility shim.
+ * Primary CORS values are defined in config/app.php.
+ */
+
+if (!defined('CORS_ENABLED')) {
+    define('CORS_ENABLED', true);
+}
+
+if (!defined('CORS_ORIGINS')) {
+    define('CORS_ORIGINS', [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+    ]);
+}
+
+if (!defined('CORS_METHODS')) {
+    define('CORS_METHODS', ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']);
+}
+
+if (!defined('CORS_HEADERS')) {
+    define('CORS_HEADERS', ['Content-Type', 'Authorization', 'Accept']);
+}
+
+if (!defined('CORS_CREDENTIALS')) {
+    define('CORS_CREDENTIALS', true);
+}
+
+if (!defined('CORS_MAX_AGE')) {
+    define('CORS_MAX_AGE', 86400);
+}
